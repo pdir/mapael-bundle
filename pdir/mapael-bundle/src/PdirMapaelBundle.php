59f3245f80aecab3072e16d1ba36a2618f7a7f60
seibtph <seibt@pdir.de>
2018-06-21 17:36:55 +0200

    first commit

---
+++ pdir/mapael-bundle/src/PdirMapaelBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Pdir\MapaelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the bundle.
+ *
+ * @author Philipp Seibt <seibt@pdir.de>
+ */
+class PdirMapaelBundle extends Bundle
+{
+
+}
