cc9132e567ff20a69203ee08ded325ae30881eb5
seibtph <seibt@pdir.de>
2018-06-22 11:26:38 +0200

    register the bundle

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace Pdir\MapaelBundle;
+namespace Pdir\MapaelBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Pdir\MapaelBundle\PdirMapaelBundle;
 
-class ContaoManagerPlugin implements BundlePluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
